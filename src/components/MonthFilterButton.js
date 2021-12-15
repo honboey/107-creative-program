b1189ceac2837745965edbbe1cf5790ae612e0da
Hon Boey <hon@dialogicstudios.com.au>
2021-12-16 10:53:53 +1100

    Added onClick functionality to MonthFilter button

--- src/components/MonthFilterButton.js
+++ src/components/MonthFilterButton.js
@@ -1,8 +1,14 @@
 import * as React from "react"
 
 function MonthFilterButton(props) {
+    function monthFilter() {
+        console.log("clicked!")
+    }
+
     return (
-        <button type="button"
+        <button 
+            type="button"
+            onClick={monthFilter}
             className={
                 "pills-button w-5/24 lg:w-3/24 mx-1 lg:mx-4 mb-3 lg:mb-4 text-base md:text-2xl font-bold pills-button--" +
                 props.colour}>
